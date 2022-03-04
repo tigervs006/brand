00b53d7f8d7d0a63976d76ec97bf7096584820b9
Kevin <wcdma020@hotmail.com>
2022-03-04 18:55:10 +0800

    完善BaseDao文件

--- app/dao/BaseDao.php
+++ app/dao/BaseDao.php
@@ -2,7 +2,6 @@
 
 namespace app\dao;
 
-use think\Model;
 use core\basic\BaseModel;
 
 abstract class BaseDao
@@ -24,4 +23,82 @@ abstract class BaseDao
      * @return string
      */
     abstract protected function setModel(): string;
+
+    /**
+     * 获取模型
+     * @return BaseModel
+     */
+    protected function getModel(): BaseModel
+    {
+        return app()->make($this->setModel());
+    }
+
+    /**
+     * 获取当前模型主键
+     * @return string
+     */
+    protected function getPK(): string
+    {
+        return $this->getModel()->getPk();
+    }
+
+    /**
+     * 新增一条数据
+     * @return BaseModel
+     * @param array $data
+     */
+    public function saveOne(array $data): BaseModel
+    {
+        return $this->getModel()::create($data);
+    }
+
+    /**
+     * 批量新增数据
+     * @param array $data
+     * @throws \Exception
+     * @return \think\Collection
+     */
+    public function saveAll(array $data): \think\Collection
+    {
+        return $this->getModel()->saveAll($data);
+    }
+
+    /**
+     * 删除一条或多条数据
+     * @return boolean
+     * @param int|array|string $id
+     */
+    public function delete(int|array|string $id): bool
+    {
+        return $this->getModel()::destroy($id);
+    }
+
+    /**
+     * 更新一条数据
+     * @return BaseModel
+     * @param array $data
+     * @param string|null $key
+     * @param array|int|string $id
+     */
+    public function updateOne(array|int|string $id, array $data, ?string $key): BaseModel
+    {
+        if (is_array($id)) {
+            $where = $id;
+        } else {
+            $where =[is_null($key) ? $this->getPk() : $key => $id];
+        }
+        return $this->getModel()::update($data, $where);
+    }
+
+    /**
+     * 批量更新数据
+     * @return BaseModel
+     * @param array $ids
+     * @param array $data
+     * @param string|null $key
+     */
+    public function batchUpdate(array $ids, array $data, ?string $key): BaseModel
+    {
+        return $this->getModel()->whereIn(is_null($key) ? $this->getPK() : $key, $ids)->update($data);
+    }
 }
