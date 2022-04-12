e6eb23a00c6f86286aa9a85b3154e6a3115c598c
Kevin <wcdma020@hotmail.com>
2022-04-12 11:46:07 +0800

    修正正则表达式

--- app/index/route/index.php
+++ app/index/route/index.php
@@ -7,7 +7,7 @@ Route::group(function () {
     Route::rule('case/:id', 'cases/detail')->name('caseDetail');                // 案例详情
     Route::rule('industry/$', 'industry/index')->name('industry');              // 文章列表
     Route::rule('industry/:id', 'industry/detail')->name('article');            // 文章内容
-    Route::rule('product/:name', 'product/index')->name('productList');         // 商品列表
+    Route::rule('product/:name', 'product/index')->name('productList');        // 商品列表
     Route::rule('product/:id', 'product/detail')->name('productDetail');        // 商品详情
     Route::rule('support/:name', 'support/:name')->name('supportDetail');       // 视频教程
-})->option(['method' => 'get', 'https' => true])->pattern(['id' => '\d+', 'name' => '[a-zA-Z]']);
+})->option(['method' => 'get', 'https' => true])->pattern(['id' => '\d+', 'name' => '[a-zA-Z]+']);
