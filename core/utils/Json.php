efab7810d2ae4122e16dbdbbe89f713e17c4001c
Kevin <wcdma020@hotmail.com>
2022-04-25 19:34:06 +0800

    添加按AntDesignPro要求返回的字段

--- core/utils/Json.php
+++ core/utils/Json.php
@@ -12,17 +12,33 @@ use think\facade\Request;
  */
 class Json
 {
+    /**
+     * 状态码
+     * @var int
+     */
     private int $code = 200;
 
+    /**
+     * @return $this
+     * @param int $code
+     */
     public function code(int $code): self
     {
         $this->code = $code;
         return $this;
     }
 
-    public function make(int $status, string $msg, ?array $data = null): Response
+    /**
+     * 创建Response实体
+     * @return Response
+     * @param int $status 状态码
+     * @param string $msg 信息
+     * @param array|null $data data
+     * @param bool|null $success AntDesignPro
+     */
+    public function make(int $status, string $msg, ?array $data = null, ?bool $success = true): Response
     {
-        $res = compact('status', 'msg');
+        $res = compact('status', 'msg', 'success');
 
         $msg && $res['msg'] = $msg;
         $res['method'] = Request::method();
@@ -32,32 +48,54 @@ class Json
         return Response::create($res, 'json', $this->code);
     }
 
-    public function success($msg = 'ok', ?array $data = null): Response
+    /**
+     * @return Response
+     * @param string|array $msg 信息
+     * @param array|null $data  data
+     * @param bool|null $success AntDesignPro
+     */
+    public function success(string|array $msg = 'ok', ?array $data = null, ?bool $success = true): Response
     {
         if (is_array($msg)) {
             $data = $msg;
             $msg = 'ok';
         }
 
-        return $this->make(200, $msg, $data);
+        return $this->make(200, $msg, $data, $success);
     }
 
+    /**
+     * @param ...$args
+     * @return Response
+     */
     public function successful(...$args): Response
     {
         return app('json')->success(...$args);
     }
 
-    public function fail($msg = 'fail', ?array $data = null): Response
+    /**
+     * @return Response
+     * @param string|array $msg 信息
+     * @param array|null $data data
+     * @param bool|null $success AntDesignPro
+     */
+    public function fail(string|array $msg = 'fail', ?array $data = null, ?bool $success = false): Response
     {
         if (is_array($msg)) {
             $data = $msg;
             $msg = 'ok';
         }
 
-        return $this->make(400, $msg, $data);
+        return $this->make(400, $msg, $data, $success);
     }
 
-    public function status($status, $msg, $result = [])
+    /**
+     * @return mixed
+     * @param string $status 状态码
+     * @param string|array $msg 信息
+     * @param array $result data
+     */
+    public function status(string $status, string|array $msg, array $result = []): mixed
     {
         $status = strtoupper($status);
         if (is_array($msg)) {
