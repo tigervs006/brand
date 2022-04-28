09a05ddcace39b8ba692770cc2766d7a32bbdb86
Kevin <wcdma020@hotmail.com>
2022-04-28 10:57:58 +0800

    删除crumbs方法，新增strOrderFilter方法

--- app/common.php
+++ app/common.php
@@ -4,19 +4,13 @@ declare (strict_types = 1);
 // 应用公共文件
 
 /**
- * 面包屑导航
- * @return array
- * @param string $channel
+ * 排序字段字符串截取
+ * @return string
+ * @param string $str 字符串
+ * @param string|null $needle 节点
+ * @param bool|null $before_needle 节点前面/后面
  */
-function crumbs (string $channel): array
+function strOrderFilter(string $str, ?string $needle = 'end', ?bool $before_needle = true): string
 {
-    static $field = 'id, pid, name, cname';
-    /** @var app\services\channel\ChannelServices $services */
-    $services = app()->make(app\services\channel\ChannelServices::class);
-    // 获取栏目信息
-    $pinfo = $services->getOne(array('name' => $channel, 'status' => 1), $field)->toArray();
-    // 获取父栏目数据
-    $pdata = $services->getParentInfo(array($pinfo), $field);
-    // 获取面包屑导航
-    return $services->getParentCrumbs($pdata);
+    return stristr($str, $needle, $before_needle);
 }
