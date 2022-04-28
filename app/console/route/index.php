45682dc8aa5d0e8ec1d8c4958de5916ceae4c16a
Kevin <wcdma020@hotmail.com>
2022-04-29 00:20:36 +0800

    delete方法更改为post，因为delete方法不能够传递数组

--- app/console/route/index.php
+++ app/console/route/index.php
@@ -15,14 +15,14 @@ Route::group(function () {
         Route::get(':id$', 'index');            // Tag内容
         Route::get('list', 'lists');            // Tag列表
         Route::post('save', 'save');            // 新增编辑
-        Route::delete('del', 'delete');         // 删除Tag
+        Route::post('del', 'delete');           // 删除Tag
     })->prefix('tags/');
     //文章部分
     Route::group('article', function () {
         Route::get(':id$', 'index');            // 文章内容
         Route::get('list', 'lists');            // 文章列表
         Route::post('save', 'save');            // 新增编辑
-        Route::delete('del', 'delete');         // 删除文章
+        Route::post('del', 'delete');           // 删除文章
         Route::get('author', 'getAuthor');      // 文章作者
         Route::post('status', 'setStatus');     // 文章状态
     })->prefix('article/');
@@ -31,34 +31,34 @@ Route::group(function () {
         Route::get(':id$', 'index');            // 商品内容
         Route::get('list', 'lists');            // 商品列表
         Route::post('save', 'save');            // 新增编辑
-        Route::delete('del', 'delete');         // 删除商品
+        Route::post('del', 'delete');           // 删除商品
     })->prefix('product/');
     // 用户部分
     Route::group('user', function () {
         Route::get(':id$', 'index');            // 用户信息
         Route::get('list', 'lists');            // 用户列表
         Route::post('save', 'save');            // 新增编辑
-        Route::delete('del', 'delete');         // 删除用户
+        Route::post('del', 'delete');           // 删除用户
     })->prefix('user/');
     // 栏目部分
     Route::group('channel', function () {
         Route::get(':id$', 'index');            // 栏目信息
         Route::get('list', 'lists');            // 栏目列表
         Route::post('save', 'save');            // 新增编辑
-        Route::delete('del', 'delete');         // 删除栏目
+        Route::post('del', 'delete');           // 删除栏目
     })->prefix('channel/');
     // 友链部分
     Route::group('links', function () {
         Route::get(':id$', 'index');            // 友链信息
         Route::get('list', 'lists');            // 友链列表
         Route::post('save', 'save');            // 新增编辑
-        Route::delete('del', 'delete');         // 删除友链
+        Route::post('del', 'delete');           // 删除友链
     })->prefix('links/');
     // 用户权限
     Route::group('group', function () {
         Route::get(':id$', 'index');            // 用户组信息
         Route::get('list', 'lists');            // 用户组列表
         Route::post('save', 'save');            // 编辑用户组
-        Route::delete('del', 'delete');         // 删除用户组
+        Route::post('del', 'delete');           // 删除用户组
     })->prefix('group/');
 })->option(['https' => true])->pattern(['id' => '\d+', 'name' => '\w+'])->middleware(think\middleware\AllowCrossDomain::class);
