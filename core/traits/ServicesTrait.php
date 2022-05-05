ca5efaebd8885d37b4b0d72c472426998b75c0d9
Kevin <wcdma020@hotmail.com>
2022-05-05 11:24:57 +0800

    新增getList方法影射，调整getOne方法的参数类型

--- core/traits/ServicesTrait.php
+++ core/traits/ServicesTrait.php
@@ -8,14 +8,16 @@ namespace core\traits;
  * @method int saveAll(array $data) 批量新增数据
  * @method \core\basic\BaseModel saveOne(array $data) 新增一条数据
  * @method int getCount(?array $map, ?string $field = 'id') 计算数据总量
- * @method mixed getOne(array $map, ?string $field = '*') 根据条件获取单条数据
  * @method bool delete(int|array|string $id, ?string $key = null) 删除一条或多条数据
  * @method bool setInc(int $id, int $incValue, ?string $field = 'click') 自增阅读量或其它
- * @method array getColumn(string $field, ?array $where = null, string $key = '') 获取某个列的数组
+ * @method mixed getOne(array $map, ?string $field, ?array $with = []) 根据条件获取单条数据
  * @method \core\basic\BaseModel batchUpdate(array $ids, array $data, ?string $key) 批量更新数据
+ * @method array getColumn(string $field, ?array $where = null, string $key = '') 获取某个列的数组
  * @method \core\basic\BaseModel updateOne(int|array|string $id, array $data, ?string $key = null) 更新一条数据
  * @method mixed getFieldValue(string $value, string $field, ?string $valueKey, ?array $where = []) 获取某个字段值
+ * @method array|\core\basic\BaseModel|\think\Model|null get(int|string|array $id, ?string $field, ?array $with = []) 获取单条数据
  * @method array|\think\Collection getData(?array $map = null, ?array $order = ['id' => 'desc'], ?string $field = '*') 获取数据列表
+ * @method array|\think\Collection getList(int $current, int $pageSize, ?array $map, ?string $field, ?array $order = ['id' => 'desc'], ?array $with = []) 获取带分页或者有关联模型的列表
  */
 trait ServicesTrait
 {
