3df93f936e6f00093aafda67c3ac8ae7de564cf9
Kevin <wcdma020@hotmail.com>
2022-05-20 11:15:12 +0800

    chore(Kevin): 删除多余引用项

--- app/services/system/ConfigServices.php
+++ app/services/system/ConfigServices.php
@@ -2,12 +2,8 @@
 declare (strict_types = 1);
 namespace app\services\system;
 
-use think\Collection;
 use app\dao\system\ConfigDao;
 use app\services\BaseServices;
-use think\db\exception\DbException;
-use think\db\exception\DataNotFoundException;
-use think\db\exception\ModelNotFoundException;
 
 class ConfigServices extends BaseServices
 {
