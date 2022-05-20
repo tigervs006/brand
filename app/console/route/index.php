56ea7b00b95cf327edb802ecb3a29b3fd475e261
Kevin <wcdma020@hotmail.com>
2022-05-20 14:35:36 +0800

    chore(Kevin): 调整带id的路由规则

--- app/console/route/index.php
+++ app/console/route/index.php
@@ -12,14 +12,14 @@ Route::group(function () {
 Route::group(function () {
     // Tags部分
     Route::group('tags', function () {
-        Route::get(':id$', 'index');            // Tag内容
+        Route::get(':id', 'index');             // Tag内容
         Route::get('list', 'lists');            // Tag列表
         Route::post('save', 'save');            // 新增编辑
         Route::post('del', 'delete');           // 删除Tag
     })->prefix('tags.tagsController/');
-    //文章部分
+    // 文章部分
     Route::group('article', function () {
-        Route::get(':id$', 'index');            // 文章内容
+        Route::get(':id', 'index');             // 文章内容
         Route::get('list', 'lists');            // 文章列表
         Route::post('save', 'save');            // 新增编辑
         Route::post('del', 'delete');           // 删除文章
@@ -29,35 +29,35 @@ Route::group(function () {
     })->prefix('article.articleController/');
     // 商品部分
     Route::group('product', function () {
-        Route::get(':id$', 'index');            // 商品内容
+        Route::get(':id', 'index');             // 商品内容
         Route::get('list', 'lists');            // 商品列表
         Route::post('save', 'save');            // 新增编辑
         Route::post('del', 'delete');           // 删除商品
     })->prefix('product.productController/');
     // 用户部分
     Route::group('user', function () {
-        Route::get(':id$', 'index');            // 用户信息
+        Route::get(':id', 'index');             // 用户信息
         Route::get('list', 'lists');            // 用户列表
         Route::post('save', 'save');            // 新增编辑
         Route::post('del', 'delete');           // 删除用户
     })->prefix('user.userController/');
     // 栏目部分
     Route::group('channel', function () {
-        Route::get(':id$', 'index');            // 栏目信息
+        Route::get(':id', 'index');             // 栏目信息
         Route::get('list', 'lists');            // 栏目列表
         Route::post('save', 'save');            // 新增编辑
         Route::post('del', 'delete');           // 删除栏目
     })->prefix('channel.channelController/');
     // 友链部分
     Route::group('links', function () {
-        Route::get(':id$', 'index');            // 友链信息
+        Route::get(':id', 'index');             // 友链信息
         Route::get('list', 'lists');            // 友链列表
         Route::post('save', 'save');            // 新增编辑
         Route::post('del', 'delete');           // 删除友链
     })->prefix('links.linksController/');
     // 用户权限
     Route::group('group', function () {
-        Route::get(':id$', 'index');            // 用户组信息
+        Route::get(':id', 'index');             // 用户组信息
         Route::get('list', 'lists');            // 用户组列表
         Route::post('save', 'save');            // 编辑用户组
         Route::post('del', 'delete');           // 删除用户组
