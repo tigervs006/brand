9e02a7dece30f7d22ad1c360eda487d5ee053b45
Kevin <wcdma020@hotmail.com>
2022-05-27 09:17:10 +0800

    feat(Kevin): 关联JwtToken模型以读取对应用户的token

--- app/model/user/User.php
+++ app/model/user/User.php
@@ -3,8 +3,16 @@ declare (strict_types = 1);
 namespace app\model\user;
 
 use core\basic\BaseModel;
+use think\model\relation\HasOne;
 
 class User extends BaseModel
 {
+    protected $pk = 'id';
+    // 只读字段
+    protected $readonly = ['create_time'];
 
+    public function token(): HasOne
+    {
+        return $this->hasOne(JwtToken::class, 'uid', 'id')->field('uid, user');
+    }
 }
