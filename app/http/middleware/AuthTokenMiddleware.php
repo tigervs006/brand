0bbd62277c3e20bf2fc292b30708778571834bd0
Kevin <wcdma020@hotmail.com>
2022-05-27 09:12:06 +0800

    feat(Kevin): 新增AuthTokenMiddleware中间件以验证Token

---
+++ app/http/middleware/AuthTokenMiddleware.php
@@ -0,0 +1,48 @@
+<?php
+declare (strict_types = 1);
+namespace app\http\middleware;
+
+use think\Request;
+use think\Response;
+use core\utils\JwtAuth;
+use core\exceptions\AuthException;
+use core\interfaces\MiddlewareInterface;
+
+/**
+ * AuthToken中间件
+ * Class AuthTokenMiddleware
+ * @package app\http\middleware
+ */
+class AuthTokenMiddleware implements MiddlewareInterface
+{
+    /**
+     * 是否校验token
+     * @var int
+     */
+    private int $isCheckToken;
+
+    /**
+     * JwtAuth
+     * @var JwtAuth
+     */
+    private JwtAuth $jwtService;
+
+    public function __construct() {
+        $this->jwtService = app()->make(JwtAuth::class);
+        $this->isCheckToken = (int) sys_config('accessTokenCheck');
+    }
+
+    /**
+     * @return Response
+     * @param \Closure $next
+     * @param Request $request
+     */
+    public function handle(Request $request, \Closure $next): Response
+    {
+        $token = $request->header('Authorization');
+        !$token && throw new AuthException('Token have been losted!');
+        $this->isCheckToken && $this->jwtService->verifyToken(trim($token));
+
+        return $next($request);
+    }
+}
