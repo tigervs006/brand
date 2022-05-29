04ba67c98ac3fe1831255d4e879ec233e835f5bf
Kevin <wcdma020@hotmail.com>
2022-05-29 10:34:57 +0800

    feat(Kevin): 移除getPrenext方法，统一通过BaseDao类实现

--- app/dao/article/ArticleDao.php
+++ app/dao/article/ArticleDao.php
@@ -11,38 +11,4 @@ class ArticleDao extends BaseDao
     {
         return Article::class;
     }
-
-    /**
-     * 上/下一篇文章
-     * @return array
-     * @param int $id id
-     */
-    public function getPrenext(int $id): array
-    {
-        $next = $this->getModel()->where('id', '>', $id)->field('id,title')->limit(1)->select();
-        $pre = $this->getModel()->where('id', '<', $id)->field('id,title')->order('id', 'desc')->limit(1)->select();
-        if ($pre->isEmpty()) {
-            $pre = array(
-                'id' => '',
-                'title' => '已经是第一篇了'
-            );
-        } else {
-            $pre = array(
-              'id' => $pre[0]['id'],
-              'title' => $pre[0]['title']
-            );
-        }
-        if ($next->isEmpty()) {
-            $next = array(
-                'id' => '',
-                'title' => '这是最后一篇了'
-            );
-        } else {
-            $next = array(
-                'id' => $next[0]['id'],
-                'title' => $next[0]['title']
-            );
-        }
-        return compact('pre', 'next');
-    }
 }
