df1718cb5a28031f6f47685b38f6e8ae4b872d71
Kevin <wcdma020@hotmail.com>
2022-06-01 22:53:35 +0800

    chore(Kevin): 新增saveUser方法

--- app/services/user/UserServices.php
+++ app/services/user/UserServices.php
@@ -4,6 +4,7 @@ namespace app\services\user;
 
 use app\dao\user\UserDao;
 use app\services\BaseServices;
+use core\exceptions\ApiException;
 
 class UserServices extends BaseServices
 {
@@ -12,4 +13,19 @@ class UserServices extends BaseServices
         $this->dao = $dao;
     }
 
+    /**
+     * 编辑/新增用户
+     * @return mixed
+     * @param array $data data
+     * @param string $message message
+     */
+    public function saveUser(array $data, string $message): mixed
+    {
+        $id = $data['id'] ?? 0;
+        unset($data['id']); // 释放$data中的id
+        return $this->transaction(function () use ($id, $data, $message) {
+            $res = $id ? $this->dao->updateOne($id, $data, 'id') : $this->dao->saveOne($data);
+            !$res && throw new ApiException($message . '用户失败');
+        });
+    }
 }
