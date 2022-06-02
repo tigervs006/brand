a965665ba493d366d83a8f778a401610e85277b8
Kevin <wcdma020@hotmail.com>
2022-06-02 11:28:15 +0800

    chore(Kevin): login方法增加返回avatar参数

--- app/console/controller/PublicController.php
+++ app/console/controller/PublicController.php
@@ -48,7 +48,7 @@ class PublicController extends BaseController
             ? $jwtService->updateOne($userInfo['id'], ['token' => $token])
             : $jwtService->saveOne(['uid' => $userInfo['id'], 'user' => $userInfo['name'], 'token' => $token]);
 
-        return $this->json->successful('用户登录成功', ['info' => ['uid' => $userInfo['id'], 'name' => $userInfo['name'],'authorization' => $token]]);
+        return $this->json->successful('用户登录成功', ['info' => ['uid' => $userInfo['id'], 'name' => $userInfo['name'], 'avatar' => $userInfo['avatar'], 'authorization' => $token]]);
     }
 
     /**
