1e867f147bd1a1b3aebc7170f426b007ae9faf95
Kevin <wcdma020@hotmail.com>
2022-06-21 12:37:31 +0800

    chore(Kevin): 判断关键词长度，超过15个则抛出异常

--- app/services/system/ConfigServices.php
+++ app/services/system/ConfigServices.php
@@ -4,6 +4,7 @@ namespace app\services\system;
 
 use app\dao\system\ConfigDao;
 use app\services\BaseServices;
+use core\exceptions\ApiException;
 
 class ConfigServices extends BaseServices
 {
@@ -12,8 +13,12 @@ class ConfigServices extends BaseServices
         $this->dao = $dao;
     }
 
-    public function updateConfig(array $data, ?string $message = '更新'): \think\Collection
+    public function updateConfig(array $data): \think\Collection
     {
+        foreach ($data as $val) {
+            10 === $val['id'] && 15 < count(explode(',', $val['value']))
+            && throw new ApiException('【首页关键词】不得超过15个，否则会被搜索引擎判断为堆砌关键词而K站');
+        }
         return $this->dao->batchUpdateAll($data);
     }
 }
