7a15d1fa990b0fc82d4f09f1b7b8c5438673ce4f
Kevin <wcdma020@hotmail.com>
2022-06-21 09:42:32 +0800

    chore(Kevin): 调整获取系统配置的逻辑

--- app/console/config/index.php
+++ app/console/config/index.php
@@ -3,5 +3,5 @@ declare (strict_types = 1);
 
 /** @var \app\services\system\ConfigServices $services */
 $services = app()->make(\app\services\system\ConfigServices::class);
-$result = $services->getData(['status' => 1], null, 'name, value')->toArray();
+$result = $services->getData(null, null, 'name, value')->toArray();
 return array_column($result, 'value', 'name');
