781d30f53e9c231f79bbbbbe22e88631c3df4bd5
Kevin <wcdma020@hotmail.com>
2022-06-21 09:43:02 +0800

    chore(Kevin): 新增updateConfig方法以更新配置项

--- app/services/system/ConfigServices.php
+++ app/services/system/ConfigServices.php
@@ -4,11 +4,17 @@ namespace app\services\system;
 
 use app\dao\system\ConfigDao;
 use app\services\BaseServices;
+use core\exceptions\ApiException;
 
 class ConfigServices extends BaseServices
 {
     public function __construct(ConfigDao $dao)
     {
         $this->dao = $dao;
+    }
+
+    public function updateConfig(array $data, ?string $message = '更新'): \think\Collection
+    {
+        return $this->dao->batchUpdateAll($data);
     }
 }
