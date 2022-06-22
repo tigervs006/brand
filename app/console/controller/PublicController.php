dbe3a669884edf0a170118994d6a800c1466b12b
Kevin <wcdma020@hotmail.com>
2022-06-22 17:56:47 +0800

    chore(Kevin): submitForm表单提交方法

--- app/console/controller/PublicController.php
+++ app/console/controller/PublicController.php
@@ -95,4 +95,81 @@ class PublicController extends BaseController
         $fileInfo = $remove->delete($this->request->param('filePath/s', null, 'trim'));
         return $fileInfo ? $this->json->successful('File deleted successfully') : $this->json->fail('File deleted failed');
     }
+
+    /**
+     * 表单提交
+     * @return Json
+     */
+    final public function submitForm(): Json
+    {
+        $post = $this->request->post([
+            'city',
+            'email',
+            'mobile',
+            'message',
+            'company',
+            'position',
+            'username',
+            'province',
+            'district',
+        ], null, 'trim');
+
+        // 关键数据验证
+        $scene = empty($post['position']) ? 'modal' : 'basic';
+        $validator = 'app\console\validate\FormValidator.' . $scene;
+        try {
+            $this->validate($post, $validator);
+        } catch (\think\exception\ValidateException $e) {
+            throw new ApiException($e->getError());
+        }
+
+        // 留言ip
+        $ipAddress = $this->request->ip();
+        // 留言时间
+        $nowTime = date('Y-m-d H:i:s');
+        // ip转int类型
+        $post['ipaddress'] = ip2long($ipAddress);
+        // 获取留言页面
+        $post['page'] = $this->request->header('referer');
+        // 组装省市区地址
+        empty($post['position']) && $post['address'] = "{$post['province']},{$post['city']},{$post['district']}";
+
+        // 写入到数据库
+        $services = $this->app->make(\app\services\user\ClientServices::class);
+        $cid = $services->getFieldValue($post['ipaddress'], 'ipaddress', 'id');
+        $cid && $post['id'] = $cid; // 相同ip的留言将视为更新留言信息
+        $services->saveClient($post, '留言失败！请检查各项信息是否正确填写');
+
+        // 入库后发送邮件
+        if ((int) sys_config('mailService')) {
+            // 邮件模板
+            $mailBody = isset($post['position'])
+                ? /** @lang text */
+                <<<TEMPLATE
+                    姓名：{$post['username']}<br/>
+                    电话：{$post['mobile']}<br/>
+                    邮箱：{$post['email']}<br/>
+                    ip地址：{$ipAddress}<br/>
+                    留言时间：{$nowTime}<br/>
+                    留言页面：{$post['page']}<br/>
+                    留言信息：{$post['message']}
+                TEMPLATE
+                : /** @lang text */
+                <<<TEMPLATE
+                    姓名：{$post['username']}<br/>
+                    电话：{$post['mobile']}<br/>
+                    邮箱：{$post['email']}<br/>
+                    ip地址：{$ipAddress}<br/>
+                    留言时间：{$nowTime}<br/>
+                    所在城市：{$post['province']}，{$post['city']}，{$post['district']}<br/>
+                    公司名称：{$post['company']}<br/>
+                    留言页面：{$post['page']}<br/>
+                    留言信息：{$post['message']}
+                TEMPLATE;
+            (new \core\utils\MailHandler())->sendMail($mailBody);
+        }
+
+        $message = isset($cid) ? '更新信息成功' : '留言成功';
+        return $this->json->successful("{$message}，我们将会在24小时内联系您");
+    }
 }
