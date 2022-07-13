91223ef56e0aeb26ff0d5f51d25306e11b298670
Kevin <wcdma020@hotmail.com>
2022-07-13 09:41:35 +0800

    chore(Kevin): 完成商品管理全部的功能

--- app/console/controller/product/ProductController.php
+++ app/console/controller/product/ProductController.php
@@ -22,7 +22,17 @@ class ProductController extends BaseController
     }
 
     /**
-     * 新增/编辑
+     * 获取商品信息
+     * @return Json
+     */
+    final public function index(): Json
+    {
+        $info = $this->services->getOne(['id' => $this->id], null, ['detail']);
+        return null === $info ? $this->json->fail() : $this->json->successful(compact('info'));
+    }
+
+    /**
+     * 新增/编辑商品
      * @return Json
      */
     final public function save(): Json
@@ -32,9 +42,13 @@ class ProductController extends BaseController
             'pid',
             'album',
             'title',
+            'sales',
+            'stock',
+            'price',
             'special',
             'content',
             'keywords',
+            'inquiries',
             'description',
         ], null, 'trim');
 
@@ -43,6 +57,8 @@ class ProductController extends BaseController
         } else {
             $message = '新增';
             $post['click'] = mt_rand(436, 695);
+            $post['sales'] = mt_rand(682, 1869);
+            $post['inquiries'] = mt_rand(862, 2468);
         }
 
         /* 验证数据 */
@@ -55,6 +71,36 @@ class ProductController extends BaseController
         $this->services->saveProduct($post, $message);
 
         return $this->json->successful($message . '商品成功');
+    }
+
+    /**
+     * 获取商品列表
+     * @return Json
+     * fixme: 组装的搜索条件不兼容$map内的其它搜索条件
+     */
+    final public function lists(): Json
+    {
+        /** 获取搜索标题 */
+        $title = $this->request->get('title/s', null, 'trim');
+        /** 获取筛选条件 */
+        $map = $this->request->only(['pid', 'status'], 'get', 'trim');
+        /** 获取时间范围 */
+        $dateRange = $this->request->only(['dateRange'], 'get', 'trim');
+        /** 获取排序条件 */
+        $order = $this->request->only(['click', 'sales', 'stock', 'price', 'inquiries'], 'get', 'strOrderFilter');
+        if ($title || $dateRange) {
+            /** 组装标题搜索条件 */
+            $map[] = ['title', 'like', '%' . $title . '%'];
+            /** 组装时间段搜索条件 */
+            $map[] = ['create_time', 'between time', [$dateRange['dateRange'][0], $dateRange['dateRange'][1]]];
+        }
+        $list = $this->services->getList($this->current, $this->pageSize, $map ?: null, '*', $order ?: $this->order, ['channel']);
+        if ($list->isEmpty()) {
+            return $this->json->fail();
+        } else {
+            $total = $this->services->getCount($map ?: null);
+            return $this->json->successful(compact('list', 'total'));
+        }
     }
 
     /**
