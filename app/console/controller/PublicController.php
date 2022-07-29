83c2eacc70009a296ec70f9453c4f76da3637a4f
Kevin <wcdma020@hotmail.com>
2022-07-29 11:45:33 +0800

    feat(Kevin): 调整所有services类从初始化中注入以及优化抛出异常的接管类

--- app/console/controller/PublicController.php
+++ app/console/controller/PublicController.php
@@ -3,14 +3,58 @@ declare (strict_types = 1);
 namespace app\console\controller;
 
 use think\facade\Cache;
+use core\utils\JwtAuth;
 use think\response\Json;
 use core\basic\BaseController;
 use core\exceptions\ApiException;
+use core\exceptions\AuthException;
 use app\services\user\UserServices;
+use core\exceptions\UploadException;
+use app\services\user\ClientServices;
+use app\services\system\RegionServices;
 use app\services\user\JwtTokenServices;
 
 class PublicController extends BaseController
 {
+    /**
+     * JwtAuth
+     * @var JwtAuth
+     */
+    private JwtAuth $jwtAuth;
+
+    /**
+     * 用户类
+     * @var UserServices
+     */
+    private UserServices $userServices;
+
+    /**
+     * Token类
+     * @var JwtTokenServices
+     */
+    private JwtTokenServices $jwtServices;
+
+    /**
+     * 行政区域类
+     * @var RegionServices
+     */
+    private RegionServices $regionServices;
+
+    /**
+     * 客户信息类
+     * @var ClientServices
+     */
+    private ClientServices $clientServices;
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->jwtAuth = $this->app->make(JwtAuth::class);
+        $this->userServices = $this->app->make(UserServices::class);
+        $this->jwtServices = $this->app->make(JwtTokenServices::class);
+        $this->clientServices = $this->app->make(ClientServices::class);
+        $this->regionServices = $this->app->make(RegionServices::class);
+    }
 
     /**
      * 用户登录
@@ -28,26 +72,22 @@ class PublicController extends BaseController
                 ['name.require' => '用户名不得为空', 'password.require' => '密码不得为空']
             );
         } catch (\think\exception\ValidateException $e) {
-            throw new ApiException($e->getError());
+            throw new AuthException($e->getError());
         }
 
-        /** @var UserServices $userService */
-        $userService = $this->app->make(UserServices::class);
-        $userInfo = $userService->getOne(['name' => $data['name']], null, ['token']);
-        is_null($userInfo) && throw new ApiException('查无此人，用户不存在，请重新输入');
-        !$userInfo['status'] && throw new ApiException("用户：${data['name']} 已禁用");
-        !password_verify($data['password'], $userInfo['password']) && throw new ApiException('密码验证失败');
+        $userInfo = $this->userServices->getOne(['name' => $data['name']], null, ['token']);
+        is_null($userInfo) && throw new AuthException('查无此人，用户不存在，请重新输入');
+        !$userInfo['status'] && throw new AuthException("用户：${data['name']} 已禁用");
+        !password_verify($data['password'], $userInfo['password']) && throw new AuthException('密码验证失败');
         /* 更新登录时间和ip地址 */
-        $userService->updateOne($userInfo['id'], ['ipaddress' => ip2long($ipAddress), 'last_login' => time()]);
+        $this->userServices->updateOne($userInfo['id'], ['ipaddress' => ip2long($ipAddress), 'last_login' => time()]);
         /* 验证通过后签发token */
-        $token = app()->make(\core\utils\JwtAuth::class)->createToken($userInfo['id'], $userInfo['name']);
+        $token = $this->jwtAuth->createToken($userInfo['id'], $userInfo['gid'], $userInfo['name']);
 
-        /** @var JwtTokenServices $jwtService */
-        $jwtService = $this->app->make(JwtTokenServices::class);
-        /* 把token信息同步到数据库 */
+        /* 把token同步到数据库 */
         isset($userInfo['token'])
-            ? $jwtService->updateOne($userInfo['id'], ['token' => $token])
-            : $jwtService->saveOne(['uid' => $userInfo['id'], 'user' => $userInfo['name'], 'token' => $token]);
+            ? $this->jwtServices->updateOne($userInfo['id'], ['token' => $token])
+            : $this->jwtServices->saveOne(['uid' => $userInfo['id'], 'user' => $userInfo['name'], 'token' => $token]);
 
         $info = [
             'uid' => $userInfo['id'],
@@ -78,11 +118,11 @@ class PublicController extends BaseController
     {
         try {
             $upload = \core\services\UploadService::init();
-            // 设置默认值upload是因为ckeditor上传时有固定的name
+            /* 设置默认值upload是因为ckeditor上传时有固定的name */
             $fileField = $this->request->post('field/s', 'upload', 'trim');
             $uploadPath = $this->request->post('path/s', 'images/article', 'trim');
             $fileInfo = $upload->to($uploadPath)->validate()->move($fileField);
-            // 适配ckeditor要求返回的参数
+            /* 适配ckeditor要求返回的参数 */
             if ('upload' === $fileField) {
                 return json(['uploaded' => 1, 'url' => $fileInfo['fullPath']]);
             } else {
@@ -90,7 +130,7 @@ class PublicController extends BaseController
                 return $fileInfo ? $this->json->successful('File uploaded successfully', $uploadInfo) : $this->json->fail('File upload failed');
             }
         } catch (\Exception $e) {
-            throw new ApiException($e->getMessage());
+            throw new UploadException($e->getMessage());
         }
     }
 
@@ -122,7 +162,7 @@ class PublicController extends BaseController
             'company' => '未知',
         ], 'post', 'trim');
 
-        // 数据验证
+        /* 数据验证 */
         $validator = 'app\console\validate\FormValidator';
         try {
             $this->validate($post, $validator);
@@ -130,32 +170,30 @@ class PublicController extends BaseController
             throw new ApiException($e->getError());
         }
 
-        // 留言来源
+        /* 留言来源 */
         $post['source'] = 1;
-        // 留言的ip
+        /* 留言的ip */
         $ipAddress = $this->request->ip();
-        // 留言时间
+        /* 留言时间 */
         $nowTime = date('Y-m-d H:i:s');
-        // ip转int类型
+        /* ip转int类型 */
         $post['ipaddress'] = ip2long($ipAddress);
-        // 获取留言页面
+        /* 获取留言页面 */
         $post['page'] = $this->request->header('referer');
-        // 组装省市区地址
-        $regionServices = $this->app->make(\app\services\system\RegionServices::class);
-        $city_name = $regionServices->value(['cid' => $post['city']], 'name');
-        $district_name = $regionServices->value(['cid' => $post['district']], 'name');
-        $province_name = $regionServices->value(['cid' => $post['province']], 'name');
+        /* 组装省市区地址 */
+        $city_name = $this->regionServices->value(['cid' => $post['city']], 'name');
+        $district_name = $this->regionServices->value(['cid' => $post['district']], 'name');
+        $province_name = $this->regionServices->value(['cid' => $post['province']], 'name');
         $post['address'] = "{$province_name}，{$city_name}，{$district_name}";
 
-        // 写入到数据库
-        $services = $this->app->make(\app\services\user\ClientServices::class);
-        $cid = $services->value(['ipaddress' => $post['ipaddress']], 'id') ?: null;
+        /* 写入到数据库 */
+        $cid = $this->clientServices->value(['ipaddress' => $post['ipaddress']], 'id') ?: null;
         $cid && $post['id'] = $cid; // 相同ip的留言将视为更新留言信息
-        $services->saveClient($post, '留言失败！请检查各项信息是否正确填写');
+        $this->clientServices->saveClient($post, '留言失败！请检查各项信息是否正确填写');
 
-        // 入库后发送邮件
+        /* 入库后发送邮件 */
         if ((int) sys_config('mail_service')) {
-            // 邮件模板
+            /* 邮件模板 */
             $mailBody =
                 /** @lang text */
                 <<<TEMPLATE
