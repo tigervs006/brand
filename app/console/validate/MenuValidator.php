895af1a1b572c1e6180b78dce0f390ee167182ed
Kevin <wcdma020@hotmail.com>
2022-08-18 10:16:20 +0800

    chore(Kevin): 调整验证设置菜单图标业务逻辑

--- app/console/validate/MenuValidator.php
+++ app/console/validate/MenuValidator.php
@@ -17,7 +17,7 @@ class MenuValidator extends validate
         'type'                  => 'require|integer',
         'name'                  => 'require|alphaDash',
         'routes'                => 'requireIf:type,3|regex:route',
-        'icon'                  => 'requireIf:pid,0|alphaDash',
+        'icon'                  => 'requireCallBack:check_require_icon|alphaDash',
         'sort'                  => 'integer|between:1,1000',
         'paths'                 => 'require|regex:path',
         'locale'                => 'require|regex:locale',
@@ -35,7 +35,7 @@ class MenuValidator extends validate
         'paths.regex'           => '菜单路径错误，参考格式：0-1-2',
         'name.require'          => '菜单名称为必填项',
         'name.alphaDash'        => '菜单名称只能是英文字母、数字和下划线、破折号的组合',
-        'icon.requireIf'        => '请为顶级栏目菜单设置图标',
+        'icon.requireCallBack'  => '请为顶级菜单设置图标',
         'icon.alphaDash'        => '菜单图标只能是英文字母、数字和下划线、破折号的组合',
         'sort.integer'          => '菜单排序必须是正整数',
         'sort.between'          => '菜单排序数值应在1~1000之间',
@@ -43,4 +43,10 @@ class MenuValidator extends validate
         'locale.regex'          => '多语言只能是英文字母、数字和下划线、英文句号的组合',
         'authority.array'       => '菜单权限参数类型必须为数组形式'
     ];
+
+    /** 只有是菜单类型和顶级菜单时图标是必填字段 */
+    function check_require_icon($value, $data): bool
+    {
+        return !$value && 1 == $data['type'] && 0 == $data['pid'];
+    }
 }
