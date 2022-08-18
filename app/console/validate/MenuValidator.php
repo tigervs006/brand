71ea5deaa72d615af2336829f7ee188c4a31feb7
Kevin <wcdma020@hotmail.com>
2022-08-18 09:23:47 +0800

    chore(Kevin): 新增type及routes参数的验证规则

--- app/console/validate/MenuValidator.php
+++ app/console/validate/MenuValidator.php
@@ -7,13 +7,16 @@ use think\validate;
 class MenuValidator extends validate
 {
     protected $regex = [
-        'path' => '[\d?:\-]+',
-        'locale' => '[\w?:\.]+',
+        'path'      => '[\d?:\-]+',
+        'locale'    => '[\w?:\.]+',
+        'route'     => '[a-zA-Z0-9\/]+'
     ];
 
     protected $rule = [
         'pid'                   => 'require|integer',
+        'type'                  => 'require|integer',
         'name'                  => 'require|alphaDash',
+        'routes'                => 'requireIf:type,3|regex:route',
         'icon'                  => 'requireIf:pid,0|alphaDash',
         'sort'                  => 'integer|between:1,1000',
         'paths'                 => 'require|regex:path',
@@ -24,7 +27,11 @@ class MenuValidator extends validate
     protected $message = [
         'pid.require'           => '请选择上级菜单，默认为顶级菜单',
         'pid.integer'           => '上级菜单的id必须为正整数',
+        'type.require'          => '显示类型是必填字段',
+        'type.integer'          => '显示类型必须是正整数',
         'paths.require'         => '菜单路径是必填字段',
+        'routes.requireIf'      => '请为接口设置路由规则',
+        'routes.regex'          => '按钮/接口路径只能是英文字母、数字和斜杠的组合',
         'paths.regex'           => '菜单路径错误，参考格式：0-1-2',
         'name.require'          => '菜单名称为必填项',
         'name.alphaDash'        => '菜单名称只能是英文字母、数字和下划线、破折号的组合',
@@ -36,5 +43,4 @@ class MenuValidator extends validate
         'locale.regex'          => '多语言只能是英文字母、数字和下划线、英文句号的组合',
         'authority.array'       => '菜单权限参数类型必须为数组形式'
     ];
-
 }
