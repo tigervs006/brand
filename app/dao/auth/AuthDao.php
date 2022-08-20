3163ba2637ef3bf2bf0e0f06bed0100ad9538043
Kevin <wcdma020@hotmail.com>
2022-08-20 15:42:38 +0800

    chore(Kevin): 调整查询用户菜单的$where条件为闭包查询

--- app/dao/auth/AuthDao.php
+++ app/dao/auth/AuthDao.php
@@ -4,6 +4,9 @@ namespace app\dao\auth;
 
 use app\dao\BaseDao;
 use app\model\auth\AuthModel;
+use core\exceptions\ApiException;
+use think\db\exception\DbException;
+use think\db\exception\DataNotFoundException;
 
 class AuthDao extends BaseDao
 {
@@ -16,10 +19,17 @@ class AuthDao extends BaseDao
     /**
      * 查询用户菜单
      * @param string $ids ids
+     * @param ?array $where 条件
      * @return \think\Collection
      */
-    public function queryMenu(string $ids): \think\Collection
+    public function queryMenu(string $ids, ?array $where = []): \think\Collection
     {
-        return $this->getModel()->whereIn('id', $ids)->order('id', 'asc')->select();
+        try {
+            return $this->getModel()->whereIn('id', $ids)->when(count($where), function ($query) use ($where) {
+                $query->where($where);
+            })->order('id', 'asc')->select();
+        } catch (DataNotFoundException|DbException $e) {
+            throw new ApiException($e->getMessage());
+        }
     }
 }
