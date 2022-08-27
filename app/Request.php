e560c67408c353afe282f9a5cc78a0d8ee1bd2ee
Kevin <wcdma020@hotmail.com>
2022-08-27 15:35:57 +0800

    chore(Kevin): 调整统一从tokenInfo方法获取并解决token

--- app/Request.php
+++ app/Request.php
@@ -1,8 +1,25 @@
 <?php
 namespace app;
 
-// 应用请求对象类
+use core\utils\JwtAuth;
+use core\exceptions\AuthException;
+
+/**
+ * Class Request
+ * @package app
+ */
 class Request extends \think\Request
 {
-
+    /**
+     * 获取并解析token
+     * @return array
+     */
+    public function tokenInfo(): array
+    {
+        $token = $this->header('Authorization');
+        $jwtServices = app()->make(JwtAuth::class);
+        return $token
+            ? $jwtServices->parseToken($token)
+            : throw new AuthException('Token is missing or incorrect');
+    }
 }
