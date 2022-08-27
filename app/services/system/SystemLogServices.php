5bd299c8d316db0660cae71abd1a91c499844215
Kevin <wcdma020@hotmail.com>
2022-08-27 16:19:35 +0800

    chore(Kevin): 调整操作日志记录逻辑，便于从其它地方可以手动写入日志

--- app/services/system/SystemLogServices.php
+++ app/services/system/SystemLogServices.php
@@ -2,39 +2,36 @@
 declare (strict_types = 1);
 namespace app\services\system;
 
-use core\utils\JwtAuth;
 use app\services\BaseServices;
 use app\dao\system\SystemLogDao;
 
 class SystemLogServices extends BaseServices
 {
-    private JwtAuth $jwtServices;
-
-    public function __construct(JwtAuth $jwtAuth, SystemLogDao $dao)
+    public function __construct(SystemLogDao $dao)
     {
         $this->dao = $dao;
-        $this->jwtServices = $jwtAuth;
     }
 
     /**
      * 记录操作日志
      * @return void
-     * @param $request
-     * @param string $token
+     * @param array $token token信息
+     * @param int|null $level 日志级别
+     * @param string|null $action 操作描述
      */
-    public function actionLogRecord($request, string $token): void
+    public function actionLogRecord(array $token, ?int $level = 3, ?string $action = null): void
     {
         $app = app('http')->getName();
-        $method = $request->rule()->getMethod();
-        $options = $request->rule()->getOption();
-        $tokenInfo = $this->jwtServices->parseToken($token);
+        $method = request()->rule()->getMethod();
+        $options = request()->rule()->getOption();
         /* 只记录post方式的日志 */
         'post' === $method && $this->dao->saveOne([
-            'uid' => $tokenInfo['uid'],
-            'gid' => $tokenInfo['gid'],
-            'action' => $options['route_name'],
-            'ipaddress' => ip2long($request->ip()),
-            'path' => $app . '/' . $request->rule()->getRule(),
+            'level' => $level,
+            'uid' => $token['uid'],
+            'gid' => $token['gid'],
+            'ipaddress' => ip2long(request()->ip()),
+            'action' => $action ?: $options['route_name'],
+            'path' => $app . '/' . request()->rule()->getRule(),
         ]);
     }
 }
