95ed16d0ecf475c4f1d402b06ec8141114b08b5e
Kevin <wcdma020@hotmail.com>
2022-08-27 15:28:38 +0800

    chore(Kevin): 调整验证用户路由权限逻辑

--- app/http/middleware/AuthCheckMiddleware.php
+++ app/http/middleware/AuthCheckMiddleware.php
@@ -2,32 +2,25 @@
 
 namespace app\http\middleware;
 
-use think\Request;
+use app\Request;
 use core\utils\JwtAuth;
-use core\exceptions\AuthException;
 use app\services\auth\AuthServices;
 use core\interfaces\MiddlewareInterface;
 
 class AuthCheckMiddleware implements MiddlewareInterface
 {
-    private JwtAuth $jwtServices;
-
     private AuthServices $authServices;
 
-    public function __construct(JwtAuth $jwtAuth, AuthServices $authServices)
+    public function __construct(AuthServices $authServices)
     {
-        $this->jwtServices = $jwtAuth;
         $this->authServices = $authServices;
     }
 
     public function handle(Request $request, \Closure $next)
     {
         /* 获取当前请求的token */
-        $token = $request->header('Authorization');
-        !$token && throw new AuthException('Token is missing or incorrect');
-        /* 解析当前请求的token */
-        $tokenInfo = $this->jwtServices->parseToken($token);
-        $this->authServices->verifyAuthority($request, $tokenInfo['gid']);
+        $token = $request->tokenInfo();
+        $this->authServices->verifyAuthority($token);
         return $next($request);
     }
 }
--- app/services/auth/AuthServices.php
+++ app/services/auth/AuthServices.php
@@ -34,17 +34,17 @@ class AuthServices extends BaseServices
     /**
      * 验证用户权限
      * @return void
-     * @param  $request
-     * @param int $gid 用户组id
+     * @param array $token
+     * @throws \Throwable
      */
-    public function verifyAuthority($request, int $gid): void
+    public function verifyAuthority(array $token): void
     {
         /* 获取当前访问路由 */
-        $rules = $request->rule()->getRule();
+        $rules = request()->rule()->getRule();
         $groupServices = app()->make(GroupServices::class);
-        $groupRole = $groupServices->value(['id' => $gid], 'menu');
+        $groupRole = $groupServices->value(['id' => $token['gid']], 'menu');
         /* 缓存用户组ID为KEY的路由权限 */
-        $roleMenu = Cache::remember($gid . '_role_menu', function () use ($groupRole) {
+        $roleMenu = Cache::remember($token['gid'] . '_role_menu', function () use ($groupRole) {
             return $this->queryMenu($groupRole, ['type' => 3])->toArray();
         }, 3600 * 24 * 7);
         if (!in_array($rules, array_column($roleMenu, 'routes'))) {
