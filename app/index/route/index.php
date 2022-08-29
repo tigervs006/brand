5df6146c7fb71877eccd129b8b9ec6300607c504
Kevin <wcdma020@hotmail.com>
2022-08-29 08:17:13 +0800

    chore(Kevin): 新增路由规则

--- app/index/route/index.php
+++ app/index/route/index.php
@@ -19,5 +19,43 @@ Route::group(function () {
     Route::rule('product/:id', 'product/detail')->name('productDetail');        // 商品详情
     Route::rule('product/<name?>', 'product/index')->name('productList');       // 商品列表
     Route::rule('support/<name?>', 'support/:name')->name('supportDetail');     // 视频教程
-    Route::miss(function() { return '404 Not Found!'; });                                         // miss路由
-})->option(['method' => 'get', 'https' => true])->pattern(['id' => '\d+', 'name' => '[a-zA-Z]+']);
+    /* 标签模型伪静态 */
+//    Route::group('tags', function () {
+//        Route::rule('$', 'tags/index');
+//        Route::rule('<tid>$', 'tags/list')->option(['ext' => 'html']);
+//    });
+    /* 单页模型伪静态 */
+//    Route::rule('single/<tid>$', 'single/index')->option(['ext' => 'html']);
+    /* 视频模型伪静态 */
+//    Route::group('video', function () {
+//        Route::rule('<tid>$', 'video/list');
+//        Route::rule('<dirname>/<aid>$', 'video/index')->option(['ext' => 'html']);
+//    });
+    /* 文档模型伪静态 */
+//    Route::group('article', function () {
+//        Route::rule('<tid>$', 'article/list');
+//        Route::rule('<dirname>/<aid>$', 'article/index')->option(['ext' => 'html']);
+//    });
+    /* 商品模型伪静态 */
+//    Route::group('product', function () {
+//        Route::rule('<tid>$', 'product/list');
+//        Route::rule('<dirname>/<aid>$', 'product/index')->option(['ext' => 'html']);
+//    });
+    /* 图集模型伪静态 */
+//    Route::group('images', function () {
+//        Route::rule('<tid>$', 'images/list');
+//        Route::rule('<dirname>/<aid>$', 'images/index')->option(['ext' => 'html']);
+//    });
+    /* 下载模型伪静态 */
+//    Route::group('download', function () {
+//        Route::rule('<tid>$', 'download/list');
+//        Route::rule('<dirname>/<aid>$', 'download/index')->option(['ext' => 'html']);
+//    });
+    /* 留言模型伪静态 */
+//    Route::group('guestbook', function () {
+//        Route::rule('<tid>$', 'guestBook/list');
+//        Route::rule('<dirname>/<aid>$', 'guestBook/index')->option(['ext' => 'html']);
+//    });
+    /* miss路由 */
+    Route::miss(function() { return '404 Not Found!'; });
+})->option(['method' => 'get', 'https' => true])->pattern(['id' => '\d+', 'tid' => '\w+', 'aid' => '\d+', 'dirname' => '\w+']);
