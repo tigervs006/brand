16f47bd534218bb4381fd7efb5c3120243c390f8
Kevin <wcdma020@hotmail.com>
2022-09-05 17:05:26 +0800

    chore(Kevin): 调整首页文档、商品的获取逻辑，删除冗余方法

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -2,10 +2,12 @@
 declare (strict_types = 1);
 namespace app\index\controller;
 
+use think\Collection;
 use think\response\Json;
 use core\basic\BaseController;
 use app\services\system\RegionServices;
 use app\services\article\ArticleServices;
+use app\services\product\ProductServices;
 
 class Index extends BaseController
 {
@@ -13,6 +15,11 @@ class Index extends BaseController
      * @var RegionServices
      */
     private RegionServices $regionServices;
+
+    /**
+     * @var ProductServices
+     */
+    private ProductServices $productServices;
 
     /**
      * @var ArticleServices
@@ -24,36 +31,60 @@ class Index extends BaseController
      */
     private string $region_field = 'id, cid, pid, code, name';
 
+    private string $product_field = 'id, pid, title, click, album, special, description';
+
     /**
      * @var string 文档字段
      */
-    private string $article_field = 'id, title, click, litpic, author, is_head, create_time, description';
+    private string $article_field = 'id, cid, title, click, litpic, author, is_head, create_time, description';
 
     protected function initialize()
     {
         parent::initialize();
         $this->regionServices = $this->app->make(RegionServices::class);
         $this->articleServices = $this->app->make(ArticleServices::class);
+        $this->productServices = $this->app->make(ProductServices::class);
     }
 
     /**
-     * 系统环境
-     * @return string
-     */
-    final public function info(): string
-    {
-        $content = phpinfo(INFO_MODULES);
-        return $this->view::display((string) $content);
-    }
-
-    /**
-     * 文章列表
+     * 首页数据
      * @return string
      */
     final public function index(): string
     {
-        $hotArt = $this->articleServices->getList(1, 7, $this->status, $this->article_field, ['is_head' => 'desc', 'id' => 'desc']);
-        return $this->view::fetch('/index', ['hotart' => $hotArt]);
+        return $this->view::fetch('/index', ['hotart' => $this->getArticle(), 'product' => $this->getProduct()]);
+    }
+
+    /**
+     * 获取商品列表
+     * @return Collection
+     */
+    private function getProduct(): Collection
+    {
+        return $this->productServices->getList(
+            1,
+            8,
+            $this->status,
+            $this->product_field,
+            ['click' => 'desc', 'id' => 'desc'],
+            null, null, ['channel']
+        );
+    }
+
+    /**
+     * 获取文档列表
+     * @return Collection
+     */
+    private function getArticle(): Collection
+    {
+        return $this->articleServices->getList(
+            1,
+            7,
+            $this->status,
+            $this->article_field,
+            ['is_head' => 'desc', 'id' => 'desc'],
+            null, null, ['channel']
+        );
     }
 
     /**
